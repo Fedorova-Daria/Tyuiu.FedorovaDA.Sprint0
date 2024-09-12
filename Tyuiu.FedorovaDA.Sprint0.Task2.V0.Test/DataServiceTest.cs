2aadf55ef11a3edaa54613e7afea079000b92299
Дарья Федорова <fedorovada06@gmail.com>
2024-09-12 20:52:40 +0500

    Task2

---
+++ Tyuiu.FedorovaDA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.FedorovaDA.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.FedorovaDA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Даша";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Даша", res);
+        }
+    }
+}
---
+++ Tyuiu.FedorovaDA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.FedorovaDA.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Даша"));
+Console.ReadKey();
+
