e0e1ad8c846206f10757d4617696e9c9c9b133ec
Дарья Федорова <fedorovada06@gmail.com>
2024-09-14 12:11:55 +0500

    Task4

---
+++ Tyuiu.FedorovaDA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.FedorovaDA.Sprint0.Task4.V0.Lib;
+
+//Сложение
+Console.WriteLine(DataService.Addition(1, 5));
+//Вычитание
+Console.WriteLine(DataService.Substraction(15, 5));
+//Умножение
+Console.WriteLine(DataService.Multiplication(10, 10));
+//Деление
+Console.WriteLine(DataService.Division(5, 5));
+
+Console.ReadKey();
